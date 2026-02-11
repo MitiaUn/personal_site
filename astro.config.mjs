f24c17b7845bcecfb8e695c9ec1c8e8e855537a9
Mitia Unuchek <stangmit@gmail.com>
2026-02-11 09:47:05 +0100

    Configure custom domain unuchek.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://MitiaUn.github.io',
-  base: '/personal_site',
+  site: 'https://unuchek.com',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
